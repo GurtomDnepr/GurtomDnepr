3e42f1016be39116e47a53051dbff3ceb4824fec
Oxana <gavredina@gmail.com>
2019-08-10 10:41:27 +0300

    New style

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+/**
+ * Created by SYSTEM on 05.07.2019.
+ */
+$("#contactForm").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+
+    $.ajax({
+        type: "POST",
+        url: "php/form-process.php",
+        data: "name=" + name + "&email=" + email + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            }
+        }
+    });
+}
+function formSuccess(){
+    $("#contactForm")[0].reset();
+    submitMSG(true, "Message Submitted!");
+}
+function formError(){
+    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $(this).removeClass();
+    });
+}
+
+formError();
+submitMSG(false, "Did you fill in the form properly?");
+
+function submitMSG(valid, msg){
+    var msgClasses;
+    if(valid){
+        msgClasses = "h3 text-center tada animated text-success";
+    } else {
+        msgClasses = "h3 text-center text-danger";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
+$.ajax({
+    type: "POST",
+    url: "php/process.php",
+    data: "name=" + name + "&email=" + email + "&message=" + message,
+    success : function(text){
+        if (text == "success"){
+            formSuccess();
+        } else {
+            formError();
+            submitMSG(false,text);
+        }
+    }
+});
---
+++ video/Gurtom_files/app.js.Без названия
@@ -0,0 +1,12 @@
+'use strict';
+
+    $('.nav-tabs').scrollingTabs({
+        enableSwiping: true,
+        scrollToTabEdge: true,
+        disableScrollArrowsOnFullyScrolled: true
+    })
+    .on('ready.scrtabs', function() {
+        $('.tab-content').show();
+    });
+
+
